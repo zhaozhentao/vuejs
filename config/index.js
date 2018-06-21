4387e1cb3b95e058df421bd4c113f9a391a030ba
zhaozhentao <344696734@qq.com>
2018-06-21 15:05:53 +0800

    简单样式

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuejs/dist',
+    assetsPublicPath: '/vuejs/dist/',
 
     /**
      * Source Maps
