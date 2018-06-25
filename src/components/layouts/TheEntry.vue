d82859e1d80575cb0c736a51d7b40fe2f48d8922
zhaozhentao <344696734@qq.com>
2018-06-25 13:57:02 +0800

    logout

--- src/components/layouts/TheEntry.vue
+++ src/components/layouts/TheEntry.vue
@@ -2,7 +2,7 @@
   <div class="navbar-right">
     <ul v-if="auth" class="nav navbar-nav github-login">
       <li>
-        <a href="javascript:;">
+        <a v-dropdown href="javascript:;">
           <span v-if="user">
             <img v-if="user.avatar" :src="user.avatar" class="avatar-topnav">
             <span v-if="user.name">{{ user.name }}</span>
@@ -11,7 +11,7 @@
           <span class="caret"></span>
         </a>
         <ul class="dropdown-menu">
-          <li><a href="#"><i class="fa fa-sign-out text-md"></i>退出</a></li>
+          <li><a href="javascript:;" @click="logout"><i class="fa fa-sign-out text-md"></i>退出</a></li>
         </ul>
       </li>
     </ul>
@@ -27,7 +27,7 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import {mapState} from 'vuex'
 
   export default {
     name: 'TheEntry',
@@ -36,6 +36,18 @@
         'auth',
         'user'
       ])
+    },
+    methods: {
+      logout() {
+        this.$swal({
+          text: '你确定要退出吗?',
+          confirmButtonText: '退出'
+        }).then((res) => {
+          if (res.value) {
+            this.$store.dispatch('logout')
+          }
+        })
+      },
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import './directives'
 import './components'
 import store from './store'
 
+import VueSweetalert2 from './plugins/vue-sweetalert2'
+
+Vue.use(VueSweetalert2)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
