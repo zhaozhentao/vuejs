60e214adc989d421572c6d94ccb618ce056608b5
zhaozhentao <344696734@qq.com>
2018-06-26 11:38:30 +0800

    修改个人信息

--- src/router/routes.js
+++ src/router/routes.js
@@ -26,7 +26,20 @@ export default [
         path: '',
         name: 'EditProfile',
         component: () => import('@/views/users/Profile.vue'),
-        meta: { auth: true }
+        meta: {auth: true}
+      },
+      {
+        path: '/users/1/edit_avatar',
+        name: 'EditAvatar',
+        component: () => import('@/views/users/Avatar.vue'),
+        meta: {auth: true}
+      },
+      // EditPassword
+      {
+        path: '/users/1/edit_password',
+        name: 'EditPassword',
+        component: () => import('@/views/users/Password.vue'),
+        meta: {auth: true}
       }
     ]
   },
